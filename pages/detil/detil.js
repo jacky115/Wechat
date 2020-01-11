c5dfc28ada77244b49e4becf636b2f3a672d5089
Jacky <Jack1522344562@gamil.com>
2020-01-11 19:42:39 +0800

    创建登录

--- pages/detil/detil.js
+++ pages/detil/detil.js
@@ -6,30 +6,51 @@ Page({
    */
   data: {
     // 视频详情
-    videoInfo:null
+    videoInfo: null,
+    // 推荐视频
+    othersList: [],
+    
   },
- 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    let videoId=options.id;
+    let videoId = options.id;
     this.getCurrentVideo(videoId)
+    this.getOthersList(videoId)
+    // console.log(othersList)
   },
- /*根据视频的id获取视频详情*/
- getCurrentVideo(videoId){
-  let that=this
-  wx.request({url:"https://easy-mock.com/mock/5c1dfd98e8bfa547414a5278/bili/videoDetail?id="+videoId,
-  success(res){
-    console.log(res)
-    if(res.data.code===0){
-      that.setData({
-        videoInfo:res.data.data.videoInfo
-      })
-    }
-  }
-})
-},
+  /*根据视频的id获取视频详情*/
+  getCurrentVideo(videoId) {
+    let that = this
+    wx.request({
+      url: "https://easy-mock.com/mock/5c1dfd98e8bfa547414a5278/bili/videoDetail?id=" + videoId,
+      success(res) {
+        console.log(res)
+        if (res.data.code === 0) {
+          that.setData({
+            videoInfo: res.data.data.videoInfo
+          })
+        }
+      }
+    })
+  },
+  /*获取推荐视频*/
+  getOthersList(videoId) {
+    let that = this;
+    wx.request({
+      url: "https://easy-mock.com/mock/5c1dfd98e8bfa547414a5278/bili/othersList?id=" + videoId,
+      success(res) {
+        console.log(res)
+        console.log(res.data.data)
+        if (res.data.code === 0) {
+          that.setData({
+            othersList: res.data.data.othersList
+          })
+        }
+      }
+    })
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
